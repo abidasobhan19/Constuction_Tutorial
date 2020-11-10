c7518f3740b0651fb4f380dbe626dc04405bd23a
abidasobhan19 <abidasobhan19@gmail.com>
2020-11-09 23:27:56 -0800

    initial Commit

--- src/component/Index.js
+++ src/component/Index.js
@@ -4,6 +4,9 @@ import S1 from "../images/SignIn.gif"
 import createproject from "../images/createproject.gif"
 import updateproject from "../images/updateproject.gif"
 import deleteproject from "../images/deleteproject.gif"
+import projectshare from "../images/projectshare.gif"
+import taskentry from "../images/taskentry.gif"
+import workdone from "../images/workdone.gif"
 
 
 const Index = () => {
@@ -49,6 +52,33 @@ const Index = () => {
             </Typography>
 
             <img src={deleteproject} alt="fill murray" style={{ width: 230, height: 340 }} />
+
+            </Box>
+   <Box>
+            <Typography variant="h6" component="h6">
+        How to Share Project in App
+            </Typography>
+
+            <img src={projectshare} alt="fill murray" style={{ width: 230, height: 340 }} />
+
+            </Box>
+
+  <Box>
+            <Typography variant="h6" component="h6">
+        How to Entry task  in App
+            </Typography>
+
+            <img src={taskentry} alt="fill murray" style={{ width: 230, height: 340 }} />
+
+            </Box> 
+            
+            
+            <Box>
+            <Typography variant="h6" component="h6">
+        Work Done Entry Through App
+            </Typography>
+
+            <img src={workdone} alt="fill murray" style={{ width: 230, height: 340 }} />
 
             </Box>
 
